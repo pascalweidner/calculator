b2f0e328aa2f9a0bf0cd3fb52bb060f60dfbb6fd
Pascal Weidner <87229144+NobodyGames@users.noreply.github.com>
2022-01-26 21:10:39 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                GUI gui = new GUI();
+            }
+        });
+    }
+}
